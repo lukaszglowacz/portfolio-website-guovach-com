7c1f23019b6cbd69565ccc3d77fc8ea7a6473878
bakatjur <bakatjur@gmail.com>
2023-04-05 05:37:07 -0400

    Add style and animation to projects section

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,23 @@ document.getElementById("paragraph1").classList.add("active");
 function downloadPDF() {
   window.open('/assets/files/resume.pdf', '_blank');
 }
+
+
+// Projects slideshow
+let slideIndex = 0;
+const projectCards = document.querySelectorAll('.project-card');
+
+// Function to show the next slide
+function showNextSlide() {
+  // Get the current and next slide indexes
+  const currentSlideIndex = slideIndex;
+  slideIndex = (slideIndex + 1) % projectCards.length;
+  const nextSlideIndex = slideIndex;
+
+  // Add the active class to the next slide and remove it from the current slide
+  projectCards[currentSlideIndex].classList.remove('active');
+  projectCards[nextSlideIndex].classList.add('active');
+}
+
+// Automatically show the next slide every 5 seconds
+setInterval(showNextSlide, 5000);
