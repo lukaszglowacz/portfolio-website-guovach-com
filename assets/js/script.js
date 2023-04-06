8af5318439d1d18156685d83b691e0a87dcb9935
LUCAS GUOVACH <bakatjur@gmail.com>
2023-04-06 13:50:15 -0400

    Add progress bar when user scroll website

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,3 +118,15 @@ async function reloadAndScrollToTop() {
     await scrollToTop(700);
     location.reload();
   }
+
+// When the user scrolls the page, execute myFunction 
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
